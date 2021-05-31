2911bc11bf5730a415a0814f9dc8dc23b2d19053
NikolaMagdic <57407543+NikolaMagdic@users.noreply.github.com>
2021-06-01 00:42:40 +0200

    Još jedan probni commit
    Da se utvrdi da li je konačno .gitignore dobro urađen

---
+++ WebProject2021/src/beans/Tractor.java
@@ -0,0 +1,17 @@
+package beans;
+
+public class Tractor {
+	
+	private String tocak;
+// probna klasa
+	public Tractor() {}
+	
+	public String getTocak() {
+		return tocak;
+	}
+	public void setTocak(String tocak) {
+		this.tocak = tocak;
+	}
+	
+	
+}
